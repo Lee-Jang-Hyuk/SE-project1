5f33c6648acab9fbe5cd172ab842819e244594d5
Joyeongbin <yo12399@naver.com>
2023-11-07 01:17:07 +0900

    SE1-5 <main>

--- calculator.py
+++ calculator.py
@@ -1,6 +1,24 @@
 
-input<
-가나다라마바사
-나라라라ㅏ
 
-병합
+
+# 메인 함수
+def main():  
+    
+    num1 = input("간단한 계산기 입니다. 더하기, 빼기, 곱하기만 가능합니다.")
+    if num1 = ' ':#이스터에그
+    
+    operator = input
+    num2 = input
+            
+    num1, num2 = get_numbers()
+    
+    if operator == '+':
+        print("결과: ", add(num1, num2))
+    elif operator == '-':
+        print("결과: ", subtract(num1, num2))
+    elif operator == '*':
+        print("결과: ", multiply(num1, num2))
+    
+
+if __name__ == "__main__":
+    main()
