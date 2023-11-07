6534d21da097df1eb997fea1a45ae2cb80d1b9dd
hyodon <wngyehs99@jbnu.ac.kr>
2023-11-07 10:39:27 +0900

    version1-multiply

--- calculator.py
+++ calculator.py
@@ -3,12 +3,3 @@ def multiply(x, y):
         raise ValueError("Error: Please use Integer")
     result = x * y
     return result
-
-def main():
-    a = int(input())
-    b = int(input())
-    result1 = multiply(a,b)
-    print(result1)
-
-if __name__ == "__main__":
-        main()
