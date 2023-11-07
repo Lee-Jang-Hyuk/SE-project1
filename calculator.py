b91b60fe8e34b1d5044e95ad406b2286fdf50811
hyodon <wngyehs99@jbnu.ac.kr>
2023-11-07 13:03:57 +0900

    version_01

--- calculator.py
+++ calculator.py
@@ -3,3 +3,25 @@ def multiply(x, y):
         raise ValueError("Error: Please use Integer")
     result = x * y
     return result
+
+# 메인 함수
+def main():  
+    
+    num1 = input("간단한 계산기 입니다. 더하기, 빼기, 곱하기만 가능합니다.")
+    if num1 = ' ':#이스터에그
+    
+    operator = input
+    num2 = input
+            
+    num1, num2 = get_numbers()
+    
+    if operator == '+':
+        print("결과: ", add(num1, num2))
+    elif operator == '-':
+        print("결과: ", subtract(num1, num2))
+    elif operator == '*':
+        print("결과: ", multiply(num1, num2))
+    
+
+if __name__ == "__main__":
+    main()
