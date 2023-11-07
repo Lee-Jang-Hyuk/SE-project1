548383dd49e5490a5090a5d8c409051bdda03796
Joyeongbin <yo12399@naver.com>
2023-11-07 15:56:09 +0900

    SE1-7 <main>

--- calculator.py
+++ calculator.py
@@ -1,24 +1,25 @@
 
-dd
-
 # 메인 함수
-def main():  
-    
-    num1 = input("간단한 계산기 입니다. 더하기, 빼기, 곱하기만 가능합니다.")
-    if num1 = ' ':#이스터에그
-    
-    operator = input
-    num2 = input
-            
-    num1, num2 = get_numbers()
-    
-    if operator == '+':
-        print("결과: ", add(num1, num2))
-    elif operator == '-':
-        print("결과: ", subtract(num1, num2))
-    elif operator == '*':
-        print("결과: ", multiply(num1, num2))
-    
+def main():
+
+    print("간단한 계산기 입니다. 더하기, 빼기, 곱하기만 가능합니다.")
+
+    num1 = int(input())
+    # 이스터에그
+    while True:
+        operator = input()
+        if operator == '=':
+            print(num1)
+            break
+        num2 = int(input())
+
+        if operator == '+':
+            num1 = add(num1, num2)
+        elif operator == '-':
+            num1 = subtract(num1, num2)
+        elif operator == '*':
+            num1 = multiply(num1, num2)
+
 
 if __name__ == "__main__":
     main()
