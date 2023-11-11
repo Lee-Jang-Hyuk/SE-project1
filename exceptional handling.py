c6fb9a85ad8fffeda445b5e313c8866d7efcedeb
Duckduckfu <106901468+Duckduckfu@users.noreply.github.com>
2023-11-11 16:58:03 +0900

    SE1-6 <exceptional_handling>

--- exceptional handling.py
+++ exceptional handling.py
@@ -1,60 +1,78 @@
-# 더하기 함수 
+# 더하기 함수
 def add(x, y):
-    if not (isinstance(x, int)) or not (isinstance(y, int)):
-        raise ValueError("Error: Please use Integer")
-    result = x + y
-    return result
+  if not (isinstance(x, int)) or not (isinstance(y, int)):
+    raise ValueError("Error: Please use Integer")
+  result = x + y
+  return result
 
-# 빼기 함수 
+
+# 빼기 함수
 def subtract(x, y):
-      if (isinstance(x, int) and isinstance(y, int)):
-          return x - y
-      else:
-          print("ERROR!")
+  if (isinstance(x, int) and isinstance(y, int)):
+    return x - y
+  else:
+    raise ValueError("Error: Please use Integer")
+
 
 # 곱하기 함수
 def multiply(x, y):
-    if not (isinstance(x, int)) or not (isinstance(y, int)):
-        raise ValueError("Error: Please use Integer")
-    result = x * y
-    return result
+  if not (isinstance(x, int)) or not (isinstance(y, int)):
+    raise ValueError("Error: Please use Integer")
+  result = x * y
+  return result
 
-# 예외 처리
+
+# 예외 처리 - 연산자 확인
 def check_operator(operator):
-    if operator not in ('+', '-', '*'):
-        raise ValueError("Error: Invalid operator. Please use '+', '-', '*'")
+  if operator not in ('+', '-', '*'):
+    return "Error: Invalid operator. Please use '+', '-', '*'"
+  return None
+
+
+# 예외 처리 - 정수 확인
 def check_integer(value):
-    try:
-        int(value)
-    except ValueError:
-        raise ValueError("Error: Please enter an Integer")
+  try:
+    int(value)
+  except ValueError:
+    return "Error: Please enter an Integer"
+  return None
+
 
 # 메인 함수
 def main():
+  print("간단한 계산기 입니다. 더하기, 빼기, 곱하기만 가능합니다.")
 
-    print("간단한 계산기 입니다. 더하기, 빼기, 곱하기만 가능합니다.")
+  try:
+    num1 = input()
 
-    num1 = int(input())
-    
     while True:
-        operator = input()
-        
-        if operator == '=':
-            print(num1)
-            break
-        
-        check_operator(operator)  # 예외 처리
+      operator = input()
+      num2 = input()
 
-        num2 = int(input())
-        check_integer(num2)      # 예외 처리
+      if operator == '=':
+        error_message_operator = check_operator(operator)
+        error_message_num1 = check_integer(num1)
+        error_message_num2 = check_integer(num2)  # 연산자 예외 처리
+        if error_message_num1:
+          raise ValueError(error_message_num1)
+        elif error_message_num2:
+          raise ValueError(error_message_num2)
+        elif error_message_operator:
+          raise ValueError(error_message_operator)
+          
+        print(num1)
+        break
 
-        if operator == '+':
-            num1 = add(num1, num2)
-        elif operator == '-':
-            num1 = subtract(num1, num2)
-        elif operator == '*':
-            num1 = multiply(num1, num2)
+      if operator == '+':
+        num1 = add(num1, num2)
+      elif operator == '-':
+        num1 = subtract(num1, num2)
+      elif operator == '*':
+        num1 = multiply(num1, num2)
+
+  except ValueError as e:
+    print(f"Error: {e}")
 
 
 if __name__ == "__main__":
-    main()
+  main()
